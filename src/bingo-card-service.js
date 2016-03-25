5a7a249874a22c8a206756f332a86bc605c93fd5
townsean <ashley.grenon@gmail.com>
2016-03-24 23:07:40 -0500

    Refactored bingo-data to bingo-card-service

---
+++ src/bingo-card-service.js
@@ -0,0 +1,40 @@
+import { inject } from 'aurelia-framework';
+import { HttpClient } from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class BingoCardService {
+    constructor(http) {
+        http.configure(config => {
+            config
+                .withBaseUrl('./src');
+        });
+        
+        this.http = http;
+    }
+    
+    getAll() {
+        return this.http.fetch('/bingo-themes.json')
+                        .then(response => response.json())
+                        .then(themes => this.themes = themes);
+    }
+    
+    getById(id) {
+        let promise = new Promise((resolve, reject) => {
+            this.getAll()
+                .then(themes => { 
+                    for (let theme of themes) {            
+                        if( theme.id === id ) {           
+                            resolve(theme);
+                        }
+                    }
+                })
+        });
+        
+        return promise;   
+    }
+    
+    getBingoCard(words) {
+        
+    }
+}
