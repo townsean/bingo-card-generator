7a59024374aff3d4d324bd202b2d55a01808f56f
townsean <ashley.grenon@gmail.com>
2016-03-30 22:34:28 -0500

    Started style for bingo cards

--- src/bingo-card.js
+++ src/bingo-card.js
@@ -3,4 +3,6 @@ import { customElement, bindable } from 'aurelia-framework';
 @customElement('bingo-card')
 export class BingoCard {
     @bindable data;
+    @bindable heading = "Sample Heading";
+    @bindable subheading = "Sample subheading";
 }
