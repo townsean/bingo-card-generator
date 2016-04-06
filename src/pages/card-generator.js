fd72d619568f74fdc773d8fe168b7ccb9dd5700c
townsean <ashley.grenon@gmail.com>
2016-04-05 22:36:58 -0500

    Refactored card creation to a method so it can be called multiple times

--- src/pages/card-generator.js
+++ src/pages/card-generator.js
@@ -1,22 +1,52 @@
-import { inject } from "aurelia-framework";
+import { inject, BindingEngine } from "aurelia-framework";
 import { BingoCardService } from '../service/bingo-card-service';
 
 @inject(BingoCardService)
+@inject(BindingEngine)
 export class CardGenerator { 
     cardCount = 15;
        
-    constructor(bingoCardService) {
+    constructor(bingoCardService, bindingEngine) {
         this.bingoCardService = bingoCardService;   
+        
+        // this.subscription = bindingEngine.propertyObserver(this, 'cardCount')
+        //                                  .subscribe( (newValue, oldValue) => {
+        //                                     this.cards = this.getGeneratedCards();
+        //                                 });
     }
     
+    /*
+    *
+    */
     activate(params) {
         return this.bingoCardService.getBingoThemeById(Number(params.id))
                                     .then(theme => this.theme = theme)
                                     .then( () => {
-                                        this.cards = [];
-                                        for(let i = 0; i < this.cardCount; i++) {
-                                            this.cards[i] = this.bingoCardService.getBingoCardData(this.theme.words);
-                                        }
+                                        this.cards = this.getGeneratedCards();
                                     });
+    }
+    
+    deactivate() {
+        // this.subscription.dispose();
+    }
+    
+    /*
+    *
+    */
+    getGeneratedCards() {
+        let cards = [];
+        
+        for(let i = 0; i < this.cardCount; i++) {
+            cards[i] = this.bingoCardService.getBingoCardData(this.theme.words);
+        }
+        
+        return cards;
+    }
+    
+    /*
+    *
+    */
+    print () {
+        
     }
 }
