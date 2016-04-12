fd55cf5eb5d3e43867f7c65e3fb1ead8b3117bbd
townsean <ashley.grenon@gmail.com>
2016-04-12 16:51:06 -0500

    Created a home page with a blurb on what the web app will accomplish for the user.

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ export class App {
   configureRouter(config, router) {
       config.title = "Bingo Card Generator";
       config.map([
-          { route: ['', 'themes'], name: 'themes', moduleId: './pages/themes', nav: true, title: 'Home' },
+          { route: ['', 'home'], name: 'home', moduleId: './pages/home', nav: true, title: 'Home'},
+          { route: 'themes', name: 'themes', moduleId: './pages/themes', nav: false, title: 'Themes' },
           { route: 'about', name: 'about', moduleId: './pages/about', nav: true, title: 'About' },
           { route: 'contact', name: 'contact', moduleId: './pages/contact', nav: true, title: 'Contact' },
           { route: 'card-generator/:id', name: 'card-generator', moduleId: './pages/card-generator', nav: false },
-          { route: 'random-drawing/:id', name: 'random-drawing', moduleId: './pages/random-drawing', nav: false }
+          { route: 'random-drawing/:id', name: 'random-drawing', moduleId: './pages/random-drawing', nav: false },
+          { route: 'custom-theme', name: 'custom-theme', moduleId: './pages/custom-theme', nav: false, title: 'Custom Theme' }
       ]);
       
       this.router = router;
